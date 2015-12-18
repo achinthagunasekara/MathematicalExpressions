341a399b7c9274c55bd6f5c49630ceaf6139587d
Archie Gunasekara <achintha@me.com>
2015-12-18 12:00:55 +1100

    Removed unused white spaces

--- src/com/achinthagunasekara/mathematical/BadMathematicalExpressionException.java
+++ src/com/achinthagunasekara/mathematical/BadMathematicalExpressionException.java
@@ -6,29 +6,29 @@ import javax.script.ScriptException;
  *
  * @author Achintha Gunasekara
  * @data 2015.10.23
- * 
+ *
  */
 public class BadMathematicalExpressionException extends Exception {
-    
+
     private final ScriptException scriptEx;
     private final String expression;
-    
+
     public BadMathematicalExpressionException(ScriptException scriptEx, String expression) {
-        
+
         super(scriptEx.getMessage());
         this.scriptEx = scriptEx;
         this.expression = expression;
     }
-    
+
     @Override
     public String toString() {
-        
+
         String message = "Unable to evaluate the expression : " + this.expression;
         return message;
     }
-    
+
     public String getErrorDetails() {
-        
+
         return this.scriptEx.getMessage();
     }
 }
